5dae918186bec84156e7b738fbd13d6303ab0716
xhesica <23373296@buaa.edu.cn>
2025-09-05 14:14:37 +0800

    use get pass to hide password input

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
+import getpass
 from login import login
 from BoyaSelect import BoyaSelect
 
@@ -14,9 +15,10 @@ login_url="https://d.buaa.edu.cn/https/77726476706e69737468656265737421f2ee4a9f6
 
 if __name__=='__main__':
     print("博雅自动选课准备中")
-    username=input("请输入学号")
-    password=input("请输入密码")
-    driver = login(username,password,login_url)
+    username = input("请输入学号")
+    password = getpass.getpass("请输入密码（输入时不会显示）：")
+
+    driver = login(username, password, login_url)
     if driver is None:
         print("登录失败，程序结束")
         exit(1)
